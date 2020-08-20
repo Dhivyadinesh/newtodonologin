f841574cda200614721e6325ba8f555f69becb59
dinesh <dinesh@dinesh-PC>
2020-08-20 23:12:19 +0530

    FIRST COMMIT

---
+++ todonologin/src/main/java/com/dd/TodonologinApplication.java
@@ -0,0 +1,13 @@
+package com.dd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodonologinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodonologinApplication.class, args);
+	}
+
+}
---
+++ todonologin/src/test/java/com/dd/TodonologinApplicationTests.java
@@ -0,0 +1,12 @@
+package com.dd;
+
+import org.junit.Test;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+class TodonologinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
